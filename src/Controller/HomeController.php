ee12118762b8b4b40971bd0d4bda22f009a1dd79
Anthony264269 <tony264269@gmail.com>
2024-03-12 13:24:26 +0100

    affichage job

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\JobType;
+use App\Repository\JobRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,12 +11,14 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(JobRepository $jobRepository): Response
     {
+        $jobs = $jobRepository ->findAll();
         $form = $this->createForm(JobType::class);
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'form' => $form->createView(),
+            'jobs' => $jobs
         ]);
     }
 }
--- src/Controller/JobsController.php
+++ src/Controller/JobsController.php
@@ -2,17 +2,35 @@
 
 namespace App\Controller;
 
+use App\Entity\Job;
+use App\Form\JobType;
+use App\Repository\JobRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+
 class JobsController extends AbstractController
 {//liste de tous les jobs
-    #[Route('/jobs', name: 'app_jobs')]
-    public function index(): Response
+    // #[Route('/jobs', name: 'app_jobs')]
+    // public function index(JobRepository $jobRepository): Response
+    // {   
+   
+    //     return $this->render('jobs/index.html.twig', [
+    //         'controller_name' => 'JobsController',
+    //     ]);
+    // }
+
+    #[Route('/job', name: 'app_jobs', methods: ['GET', 'POST'])]
+    public function index( JobRepository $jobRepository): Response
     {
+
+        $jobs = $jobRepository ->findAll();
         return $this->render('jobs/index.html.twig', [
-            'controller_name' => 'JobsController',
+            'jobs' => $jobs,
+            
         ]);
     }
 // deuxieme rout e pour le detail d' un job!!!!!!!!!!!!!!!!!!!!!!!!!A FAIRE PAR L4 ID JE PENSE
